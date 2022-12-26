6a8b0f2170c95ee80f59427de5c09a0a8e90dfdb
KULDEEP-18102000 <jadonkuldeepsingh2@gmail.com>
2022-12-26 22:29:36 +0530

    yash sir approach

--- task-12.js
+++ task-12.js
@@ -4,9 +4,22 @@ const emailInput = document.querySelector('#email');
 const ul=document.getElementById('users');
 // console.log(ul);
 
-let myObj_arr=[];
-let myObj_serialized=JSON.stringify(myObj_arr);
-localStorage.setItem('myObj',myObj_serialized);
+// let myObj_arr=[];
+// let myObj_serialized=JSON.stringify(myObj_arr);
+// localStorage.setItem('myObj',myObj_serialized);
+while(ul.hasChildNodes()){
+    ul.removeChild(ul.firstChild);
+}
+for(var i=0;i<localStorage.length;i++){
+    var name=JSON.parse(localStorage.getItem(localStorage.key(i))).username;
+    // console.log(name);
+    var email=JSON.parse(localStorage.getItem(localStorage.key(i))).userEmail;
+    // console.log(email);
+    const li=document.createElement('li');
+    const s=`${name} ${email}`;
+    li.appendChild(document.createTextNode(s))
+    ul.appendChild(li);
+}
 myForm.addEventListener('submit', onSubmit);
 
 
@@ -18,42 +31,19 @@ function onSubmit(e) {
         username:nameInput.value,
         userEmail:emailInput.value
     };
-    myObj_arr_serialized=localStorage.getItem('myObj');
-    myObj_arr=JSON.parse(myObj_arr_serialized);
-    myObj_arr.push(myObj);
-    let myObj_serialized=JSON.stringify(myObj_arr);
-    localStorage.setItem('myObj',myObj_serialized);
-    let myObj_deserialized=JSON.parse(localStorage.getItem('myObj'));
-    console.log(myObj_deserialized);
-    const n=myObj_deserialized.length;
+    myObj_serialized=JSON.stringify(myObj);
+    localStorage.setItem(myObj.userEmail,myObj_serialized);
     while(ul.hasChildNodes()){
         ul.removeChild(ul.firstChild);
     }
-    for(let i=0;i<n;i++){
-        let username=myObj_deserialized[i].username;
-        // console.log(username);
-        let userEmail=myObj_deserialized[i].userEmail;
-        // console.log(userEmail);
-        let s=`username->${username} and useremail->${userEmail}`
+    for(var i=0;i<localStorage.length;i++){
+        var name=JSON.parse(localStorage.getItem(localStorage.key(i))).username;
+        // console.log(name);
+        var email=JSON.parse(localStorage.getItem(localStorage.key(i))).userEmail;
+        // console.log(email);
         const li=document.createElement('li');
-        li.appendChild(document.createTextNode(s));
+        const s=`${name} ${email}`;
+        li.appendChild(document.createTextNode(s))
         ul.appendChild(li);
     }
-    console.log(n);
-    localStorage.setItem('name',nameInput.value);
-    localStorage.setItem('email',emailInput.value);
   }
-
-// let myObj_deserialized=JSON.parse(localStorage.getItem('myObj'));
-// console.log(myObj_deserialized);
-// const n=myObj_deserialized.length;
-// for(let i=0;i<n;i++){
-//     let username=myObj_deserialized[i].username;
-//     // console.log(username);
-//     let userEmail=myObj_deserialized[i].userEmail;
-//     // console.log(userEmail);
-//     let s=`username->${username} and useremail->${userEmail}`
-//     const li=document.createElement('li');
-//     li.appendChild(document.createTextNode(s));
-//     ul.appendChild(li);
-//     }
