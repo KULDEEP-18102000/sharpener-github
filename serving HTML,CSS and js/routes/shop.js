d04d40ce1de9e59ff74a450cc1f6f40af23b6dba
KULDEEP-18102000 <jadonkuldeepsingh2@gmail.com>
2023-02-03 18:48:58 +0530

    express-task-6

---
+++ serving HTML,CSS and js/routes/shop.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+});
+
+router.get('/contactus',(req,res,next)=>{
+  res.sendFile(path.join(rootDir, 'views', 'contact.html'));
+})
+
+router.post('/success',(req,res,next)=>{
+  res.sendFile(path.join(rootDir, 'views', 'success.html'));
+})
+
+module.exports = router;
