a8c70884b43f9efdefb61d576e83a73dd665d1f4
KULDEEP-18102000 <jadonkuldeepsingh2@gmail.com>
2023-02-04 10:41:45 +0530

    sharpener-task-7

--- serving HTML,CSS and js/routes/shop.js
+++ serving HTML,CSS and js/routes/shop.js
@@ -1,8 +1,9 @@
-const path = require('path');
-
 const express = require('express');
 
 const rootDir = require('../util/path');
+
+const contactController=require('../controllers/contact')
+const successController=require('../controllers/success')
 
 const router = express.Router();
 
@@ -10,12 +11,8 @@ router.get('/', (req, res, next) => {
   res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
-router.get('/contactus',(req,res,next)=>{
-  res.sendFile(path.join(rootDir, 'views', 'contact.html'));
-})
+router.get('/contactus',contactController.getContact)
 
-router.post('/success',(req,res,next)=>{
-  res.sendFile(path.join(rootDir, 'views', 'success.html'));
-})
+router.post('/success',successController.getSuccess)
 
 module.exports = router;
