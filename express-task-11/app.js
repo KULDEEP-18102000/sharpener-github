80db0f1eaf6ef0ada38a81d2b5560cde49bbf2e6
KULDEEP-18102000 <jadonkuldeepsingh2@gmail.com>
2023-02-09 19:17:17 +0530

    sharpener task-15

--- express-task-11/app.js
+++ express-task-11/app.js
@@ -3,6 +3,9 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const db=require('./util/database')
+
+
 const errorController = require('./controllers/error');
 
 const app = express();
@@ -12,6 +15,8 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+db.execute('SELECT * FROM products');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
