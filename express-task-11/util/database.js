c81226e246978dcc63c6fe49530947c27a9d5e37
KULDEEP-18102000 <jadonkuldeepsingh2@gmail.com>
2023-02-10 20:37:21 +0530

    express task-17

--- express-task-11/util/database.js
+++ express-task-11/util/database.js
@@ -1,10 +1,8 @@
-const mysql=require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool=mysql.createPool({
-    host:'localhost',
-    user:'root',
-    database:'node-complete',
-    password:'kuldeepjadon@18'
-})
+const sequelize = new Sequelize('node-complete', 'root', 'kuldeepjadon@18', {
+  dialect: 'mysql',
+  host: 'localhost'
+});
 
-module.exports=pool.promise();
+module.exports = sequelize;
