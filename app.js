60e1b1a938e47dc036b92043e460045acf87896c
KULDEEP-18102000 <jadonkuldeepsingh2@gmail.com>
2023-03-31 09:48:53 +0530

    implemented get all products, update and delete product by mongoose

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 
-const mongoConnect=require('./util/database').mongoConnect
-const User = require('./models/user');
+// const mongoConnect=require('./util/database').mongoConnect
+const mongoose=require('mongoose')
+// const User = require('./models/user');
 
 
 const app = express();
@@ -20,21 +21,30 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('63f7b6e16e30b21bda9c4ec1')
-    .then(user => {
-      req.user = new User(user.name,user.email,user.cart,user._id);
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('63f7b6e16e30b21bda9c4ec1')
+//     .then(user => {
+//       req.user = new User(user.name,user.email,{items:[]},user._id);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(()=>{
+// mongoConnect(()=>{
   // console.log(client)
+  // app.listen(3000)
+// })
+
+mongoose.connect('mongodb+srv://Boby:RmL7Bs5Od0JGlSEP@cluster0.scdgo.mongodb.net/shop?retryWrites=true&w=majority')
+.then(result=>{
+  console.log("connected")
   app.listen(3000)
+})
+.catch(err=>{
+  console.log(err)
 })
